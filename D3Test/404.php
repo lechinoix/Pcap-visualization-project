fad8352ed28fa1a6385b788b1b4c1ea790b291f4
Le_chinoix <nicolas.ngomai@gmail.com>
2016-01-17 20:51:55 +0100

    Our tests on D3.js

---
+++ D3Test/404.php
@@ -0,0 +1,2 @@
+<h1>404 error</h1>
+<p>Wrong URL</p>
---
+++ D3Test/home.php
@@ -0,0 +1,8 @@
+<h1>D3 graph tests</h1>
+<p>
+	<h2>Graph took on <a href="http://d3js.org">D3js.org</a></h2>
+	<ul>
+		<li><a href="?graph=parallel">Parallel</a></li>
+		<li><a href="?graph=network">Network</a></li>
+	</ul>
+<p>
