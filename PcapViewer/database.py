830abef5e4d274a7d4f7984bb96dae527d52cd9f
h3r3tiik <thibault.lengagne@yahoo.fr>
2016-01-28 19:10:48 +0100

    modifié :         PcapViewer/database.py 	modifié :         PcapViewer/models.py 	modifié :         PcapViewer/utils/parser2.py 	nouveau fichier : Resultats/18mo/packets.csv 	nouveau fichier : Resultats/18mo/sessions.csv 	nouveau fichier : Resultats/18mo/stats.csv 	nouveau fichier : Resultats/18mo/users.csv 	nouveau fichier : Resultats/2mo/packets.csv 	nouveau fichier : Resultats/2mo/sessions.csv 	nouveau fichier : Resultats/2mo/stats.csv 	nouveau fichier : Resultats/2mo/users.csv 	nouveau fichier : Resultats/Statistiques-temps.xls 	nouveau fichier : Resultats/resultats-test1.xls 	nouveau fichier : Resultats/test1/packet-test1.csv 	nouveau fichier : Resultats/test1/sessions-test1.csv 	nouveau fichier : Resultats/test1/stats-test1.csv 	nouveau fichier : Resultats/test1/test1.pcap 	nouveau fichier : Resultats/test1/users-test1.csv

--- PcapViewer/database.py
+++ PcapViewer/database.py
@@ -15,4 +15,7 @@ def init_db():
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
     import models
+    Base.metadata.drop_all(bind=engine)
+    print "Database dropped"
     Base.metadata.create_all(bind=engine)
+    print "Database created"
--- PcapViewer/utils/parser2.py
+++ PcapViewer/utils/parser2.py
@@ -3,7 +3,7 @@ from datetime import datetime
 import json
 import sys
 from models import Stat ,User, Packet, Session
-from database import db_session
+from database import db_session,init_db
 #from netaddr import IPNetwork, IPAddress
 
 def hexdump(x):
@@ -15,46 +15,46 @@ def hexdump(x):
     return res
 
 def initiate():
-    for el in Stat.query.all():
-        db_session.delete(el)
-    liste = ["TOTAL","Autre","HTTP","HTTPS","LDAP","LDAPS","SMB","FTP","SSH","SMTP","POP3","IMAP","KERBEROS","DNS","ARP","ICMP"]
-    for el in liste:
-        S = Stat(el,0)
-        db_session.add(S)
+    init_db()
+    # for el in Stat.query.all():
+    #     db_session.delete(el)         
 
-    for el in User.query.all():
-        db_session.delete(el) 
-    for el in Session.query.all():
-        db_session.delete(el)   
-    db_session.commit()
 
 def get_protocol(pkt):
     protocol = "Autre"
     if pkt.haslayer(TCP):
         if pkt[TCP].dport == 7 or pkt[TCP].sport == 7:
             protocol = "ICMP"
-        elif pkt[TCP].dport == 80 or pkt[TCP].sport == 80:
-            protocol = "HTTP"
-        elif pkt[TCP].dport == 443 or pkt[TCP].sport == 443:
-            protocol = "HTTPS"                
-        elif pkt[TCP].dport == 445 or pkt[TCP].sport == 445:
-            protocol = "SMB"
+        elif pkt[TCP].dport == 20 or pkt[TCP].sport == 20:
+            protocol = "FTP"
         elif pkt[TCP].dport == 21 or pkt[TCP].sport == 21:
             protocol = "FTP"
         elif pkt[TCP].dport == 22 or pkt[TCP].sport == 22:
             protocol = "SSH"
         elif pkt[TCP].dport == 25 or pkt[TCP].sport == 25:
             protocol = "SMTP"
+        elif pkt[TCP].dport == 80 or pkt[TCP].sport == 80:
+            protocol = "HTTP"
         elif pkt[TCP].dport == 88 or pkt[TCP].sport == 88:
             protocol = "KERBEROS"
-        elif pkt[TCP].dport == 110 or pkt[TCP].sport == 110:
+        elif pkt[TCP].dport == 110 or pkt[TCP].dport == 109 or pkt[TCP].sport == 110 or pkt[TCP].sport == 109:
             protocol = "POP3"
-        elif pkt[TCP].dport == 110 or pkt[TCP].sport == 110:
+        elif pkt[TCP].dport == 115 or pkt[TCP].sport == 115:
+            protocol = "SFTP"
+        elif pkt[TCP].dport == 220 or pkt[TCP].dport == 143 or pkt[TCP].sport == 220 or pkt[TCP].sport == 143:
             protocol = "IMAP"
-        elif pkt[TCP].dport == 389 or pkt[TCP].sport == 110:
+        elif pkt[TCP].dport == 389 or pkt[TCP].sport == 389:
             protocol = "LDAP"
-        elif pkt[TCP].dport == 636 or pkt[TCP].sport == 110:
+        elif pkt[TCP].dport == 443 or pkt[TCP].sport == 443:
+            protocol = "HTTPS"                
+        elif pkt[TCP].dport == 445 or pkt[TCP].sport == 445:
+            protocol = "SMB"
+        elif pkt[TCP].dport == 636 or pkt[TCP].sport == 636:
             protocol = "LDAPS"
+        elif pkt[TCP].dport == 993 or pkt[TCP].sport == 993:
+            protocol = "IMAPS"
+        elif pkt[TCP].dport == 995 or pkt[TCP].sport == 995:
+            protocol = "POP3S"
     elif pkt.haslayer(UDP):
         if pkt[UDP].dport == 53 or pkt[UDP].sport == 53:
             protocol = "DNS"
@@ -64,226 +64,221 @@ def get_protocol(pkt):
         protocol = "ICMP"
     return protocol
 
-def feed_stats(packet):
+def feed_stats(stat, packet):
     protocol = get_protocol(packet)
-    S = Stat.query.filter(Stat.name==protocol).first()
-    if S != None :
-        S.value +=1
-    T = Stat.query.filter(Stat.name=="TOTAL").first()
-    if T != None :
-        T.value +=1
-    # stats["total"] +=1
-    # if stats.has_key(protocol):
-    #     stats[protocol] +=1 
-    # else:
-    #     stats[protocol] = 1  
+    stat["total"] +=1
+    if stat.has_key(protocol):
+        stat[protocol] +=1 
+    else:
+        stat[protocol] = 1  
 
-def feed_trames(trames,data,sessionId):
+#TO DO mettre a jour cette fonction
+def feed_trames(pkt,session):
 
-    try:
-        id = trames[-1]["id"]
-    except:
-        id = 0
+    timestamp = datetime.fromtimestamp(pkt.time).strftime('%Y-%m-%d %H:%M:%S')
+    protocol = get_protocol(pkt)
     
-    for pkt in data:
-        id += 1
-        packet={}
-        packet['timestamp'] = datetime.fromtimestamp(pkt.time).strftime('%Y-%m-%d %H:%M:%S')
-        packet['id'] = id
-        packet['sessionId'] = sessionId
-        protocol = get_protocol(pkt)
+    if pkt.haslayer(IP):
+        ip = pkt.getlayer(IP) 
+        hostSrc=ip.src
+        hostDest=ip.src
 
-        if pkt.haslayer(IP):
-            #stats["ip"] += 1
-            ip = pkt.getlayer(IP) 
+        if ip.haslayer(TCP):
+            tcp = ip.getlayer(TCP)
+            portSrc = tcp.sport
+            portDest = tcp.dport
+            
+        elif ip.haslayer(UDP):
+            udp = ip.getlayer(UDP)
+            portSrc = udp.sport
+            portDest = udp.dport
+            
+        else:
+            portSrc=None
+            portDest=None
+            
+    elif pkt.haslayer(ARP):
+        arp = pkt.getlayer(ARP)
+        hostSrc = arp.psrc
+        hostDest = arp.pdst  
+        portSrc = None
+        portDest = None
 
-            packet["proto"]=protocol
-            packet["IpSrc"]=ip.src
-            packet["IpDest"]=ip.dst
-
-            if ip.haslayer(TCP):
-                tcp = ip.getlayer(TCP)
-                packet["PortSrc"] = tcp.sport
-                packet["PortDst"] = tcp.dport
-                
-            elif ip.haslayer(UDP):
-                udp = ip.getlayer(UDP)
-                packet["PortSrc"] = udp.sport
-                packet["PortDst"] = udp.dport
-                
-            else:
-                packet["PortSrc"]=None
-                packet["PortDest"]=None
-                
-        elif pkt.haslayer(ARP):
-            arp = pkt.getlayer(ARP)
-            packet["proto"] = "ARP"
-            packet["src"] = ( arp.psrc , None )
-            packet["dst"] = ( arp.pdst, None )
-
-        if pkt.haslayer(Raw):
-            packet["payload"] = hexdump(pkt.getlayer(Raw))
+    if pkt.haslayer(Raw):
+        data = {"data" :hexdump(pkt.getlayer(Raw))}
+    else:
+        data = {}
         
-        trames.append(packet)
+    P = Packet(hostSrc,hostDest,portSrc,portDest,protocol,data,timestamp,session)
+    db_session.add(P)
+    #database.insert_packet(packet)
+    #res[0]["connexion"].append(packet)
 
-def extract_session(summary,sessionId):
+#TO DO Probleme, la table Session a ete supprimee et je n'arrive pas a la remttre...
+def extract_session(summary,data):
     
-    summ = summary.split(' ')
+    s = summary.split(' ')
     # summary est de la forme UDP 192.168.11.228:21893 > 208.67.222.222:
-    if summ[0]=="TCP" or summ[0]=="UDP":
-        #sess = Session(summ[1].split(":")[0], summ[3].split(":")[0], summ[1].split(":")[1], summ[3].split(":")[1],summ[0]) 
-        #db_session.add(sess)     
+    if s[0]=="TCP" or s[0]=="UDP":
+        Src = s[1].split(":")
+        Dst = s[3].split(":")
+        hostSrc = Src[0]
+        portSrc = Src[1]
+        hostDest = Dst[0]
+        portDest = Dst[1]
+        protocol = s[0]
+        if hostDest != "255.255.255.255":
+            sess = Session(hostSrc,hostDest,portSrc,portDest,protocol) 
+            db_session.add(sess) 
+            for pkt in data:
+                feed_trames(pkt,sess)
+
+        #print "Session ajoutee"
+         
+        # for packet in data:
+        #     nb += 1
+        #     if packet.haslayer(Raw):
+        #         payload += onlyascii(packet.getlayer(Raw).load) + "\n"
+        # sess["Nombre"] = nb
+        # sess["Payload"] = payload   
 
 #Probleme : le volume est toujours egal a 64...
-def feed_user(pkt):
+def feed_user(user,pkt,tid):
 
     if pkt.haslayer(IP):
         ip = pkt.getlayer(IP) 
         #if in_network(ip.src) or in_network(ip.src,"10.0.0.0/8") or in_network(ip.src,"172.16.0.0/12"):
-        protocol = get_protocol(ip)
-        #print protocol
-        U = User.query.filter(User.address==ip.src).first()
-        
-        if U != None:
-            U.exchanged["Volume"]+=sys.getsizeof(ip)
-            
-            if U.exchanged["Protocoles"].has_key(protocol):
-                U.exchanged["Protocoles"][protocol]["Volumeout"]+=sys.getsizeof(ip)
-                U.exchanged["Protocoles"][protocol]["Nombreout"]+=1
-                # print "Mise a jour :",
-                # print res,
-                # print " (" + protocol + ")"
-                res = U
-                db_session.delete(U)
-                U=User(res.address,res.exchanged)
-                db_session.add(U)
-                db_session.commit()
-            else:
-                print "Ajout d'un nouveau protocol ("+protocol+") a "+ip.src
-                U.exchanged["Protocoles"][protocol]={
-                    "Volumein": 0,
-                    "Volumeout":sys.getsizeof(ip),
-                    "Nombrein":0,
-                    "Nombreout":1                      
-                }
-                res = U
-                db_session.delete(U)
-                U = User(res.address,res.exchanged)
-                db_session.add(U)
-        else:
-            U = User(ip.src,{})
-            U.exchanged={
-                "Volume": sys.getsizeof(ip),
-                "Protocoles":
-                {
-                      protocol:
-                      {
-                        "Volumein":0,
+        if True:
+            protocol = get_protocol(ip)
+            #print protocol
+            if user.has_key(ip.src):
+                user[ip.src]["Volume"]+=sys.getsizeof(ip)
+                if user[ip.src]["Protocole"].has_key(protocol):
+                    user[ip.src]["Protocole"][protocol]["Volumeout"]+=sys.getsizeof(ip)
+                    user[ip.src]["Protocole"][protocol]["Nombreout"]+=1
+                else:
+                    #print "Ajout d'un nouveau protocol ("+protocol+") a "+ip.src
+                    user[ip.src]["Protocole"][protocol]={
+                        "Volumein": 0,
                         "Volumeout":sys.getsizeof(ip),
                         "Nombrein":0,
-                        "Nombreout":1
+                        "Nombreout":1                      
+                    }
+            else:
+                user[ip.src]={
+                    "Volume": sys.getsizeof(ip),
+                    "id":tid,
+                    "Protocole":
+                      {
+                          protocol:
+                          {
+                            "Volumein":0,
+                            "Volumeout":sys.getsizeof(ip),
+                            "Nombrein":0,
+                            "Nombreout":1
+                          }
                       }
                 }
-            }
-            db_session.add(U)
-        db_session.commit()
-            #print "Nouvel IP : "+ip.src
-        
-        U = User.query.filter(User.address==ip.dst).first()
-        #protocol = get_protocol(ip)
-        if U != None:
-            U.exchanged["Volume"]+=sys.getsizeof(ip)
-            if U.exchanged["Protocoles"].has_key(protocol):
-                U.exchanged["Protocoles"][protocol]["Volumein"]+=sys.getsizeof(ip)
-                U.exchanged["Protocoles"][protocol]["Nombrein"]+=1
-                res = U
-                db_session.delete(U)
-                U = User(res.address,res.exchanged)
-                db_session.add(U)
-            else:
-                print "Ajout d'un nouveau protocol ("+protocol+") a "+ip.dst
-                U.exchanged["Protocoles"][protocol]={
-                    "Volumein": sys.getsizeof(ip),
-                    "Volumeout":0,
-                    "Nombrein":1,
-                    "Nombreout":0
-                }
-                res = U
-                db_session.delete(U)
-                U = User(res.address,res.exchanged)
-                db_session.add(U)
-        else:
-            U = User(ip.dst,{})
-            U.exchanged={
-                "Volume": sys.getsizeof(ip),
-                "Protocoles":
-                  {
-                      protocol:
-                      {
+                #print "Nouvel IP : "+ip.src
+                tid +=1
+
+        #elif in_network(ip.dst) or in_network(ip.dst,"10.0.0.0/8") or in_network(ip.dst,"172.16.0.0/12"):
+            #protocol = get_protocol(ip)
+            if user.has_key(ip.dst):
+                user[ip.dst]["Volume"]+=sys.getsizeof(ip)
+                if user[ip.dst]["Protocole"].has_key(protocol):
+                    user[ip.dst]["Protocole"][protocol]["Volumein"]+=sys.getsizeof(ip)
+                    user[ip.dst]["Protocole"][protocol]["Nombrein"]+=1
+                else:
+                    #print "Ajout d'un nouveau protocol ("+protocol+") a "+ip.dst
+                    user[ip.dst]["Protocole"][protocol]={
                         "Volumein": sys.getsizeof(ip),
                         "Volumeout":0,
                         "Nombrein":1,
                         "Nombreout":0
+                    }
+            else:
+                user[ip.dst]={
+                    "Volume": sys.getsizeof(ip),
+                    "id":tid,
+                    "Protocole":
+                      {
+                          protocol:
+                          {
+                            "Volumein": sys.getsizeof(ip),
+                            "Volumeout":0,
+                            "Nombrein":1,
+                            "Nombreout":0
+                          }
                       }
-                  }
-            }
-            db_session.add(U)
-        db_session.commit()
+                }
+                #print "Nouvel IP : "+ip.dst
+                tid +=1
+
+    return tid 
             #print "Nouvel IP : "+ip.dst
     
+def commit(user,stat):
+    for el in user:
+        U = User(el,user[el])
+        db_session.add(U)
+    print stat
+    for el in stat:
+        S = Stat(el,stat[el])
+        db_session.add(S)
 
 def parse(filename):
     """
     Parses pcap file and populate de PostgreSQL Database
     """
     try:
-        # Stat.query.execute.drop_all()
-        # Stat.query.execute.create_all()
         initiate()
-        #print dir(db_session.class.query)
-        #db_session.Stat.class.all
     except Exception as e:
         print e
+    user={}
+    stat={"total":0}
     #try:
     ttot = time.clock()
     pcap = rdpcap(filename)
     print "Temps de lecture du pcap : " + str(time.clock()-ttot)
-
-    resultat={"trames":[]}
+    tid=0
     t0 = 0
     t1 =0
     t2= 0
     t3 = 0
+    t4 = 0
     for pkt in pcap:
         t0 = time.clock()
-        feed_user(pkt)
-        t2 += time.clock() - t0
-        feed_stats(pkt)
-    #returnjson =  json.dumps(res, sort_keys=True)
-    s = pcap.sessions()
-    sessionId = 0
-    for summary,data in s.iteritems():
-        sessionId+=1
-        t0 = time.clock()
-        extract_session(summary,sessionId)
-        t3 += time.clock()-t0
-        t0 = time.clock()
-        feed_trames(resultat["trames"],data,sessionId)
+        tid = feed_user(user,pkt,tid)
         t1 += time.clock() - t0
+        t0 = time.clock()
+        feed_stats(stat,pkt)
+        t2 += time.clock()-t0
 
-    print "Trames generated in "+str(t1)+" seconds"
-    #print json.dumps(resultat["sessions"],indent=4) 
-    print "Sessions generated in "+str(t3)+" seconds"
-    #print json.dumps(resultat["sessions"],indent=4)    
-    print "Treemap generated "+str(t2)+" seconds"
-    #print json.dumps(resultat["user"],indent=4)
+    t0 = time.clock()
+    commit(user,stat)
+    t4 += time.clock() - t0
+
+    t0 = time.clock()
+    s = pcap.sessions()
+    for summary,data in s.iteritems():
+        extract_session(summary,data)
+    t3 += time.clock()-t0    
+
+    print "Sessions & Packets generated in "+str(t3)+" seconds" 
+    print "Treemap generated "+str(t1)+" seconds"
+    #print json.dumps(user,indent=4)
+    print "Stat generated "+str(t2)+" seconds"
 
     print "Done parsing"
 # except Exception, e:
     #      return {"Error":e}
     #print "\n", web
+    t0 = time.clock()
     db_session.commit()
-    return resultat
+    t4 += time.clock()-t0
+    print "Commit took " + str(t4) + " seconds"
+    return {}
 
 #-----------------------------------UNUSED FUNCTION-----------------------------------------#
 
